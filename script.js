9ab688aa354116f45ff7595a3b159065881c712c
Thunder2953 <98075973+Thunder2953@users.noreply.github.com>
2022-12-12 17:12:37 +0000

    Pending changes exported from your codespace

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("hello i am thunder your personal assistant,  will give you a walkthrough through this website first to prove you want to learn web hacking find the flag which hidden inside this website find it and prove your skill ")
+
+
+var Flag = "TND{He||O_I-AM-NEW_TO_H4CkING}"
+
+console.log(Flag)
